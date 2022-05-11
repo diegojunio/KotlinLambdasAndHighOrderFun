612c7ec518b1846aa6b754a06871d750c4607d2b
diegojunio <diegojunio@gmail.com>
2022-05-10 21:37:54 -0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,15 @@
+import java.util.Arrays.*
+
+fun main(){
+    var dirtyLevel = 20
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+    println(updateDirty(30, waterFilter))
+    println(updateDirty(15, ::increaseDirty))
+}
+
+fun updateDirty (dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
+
+fun increaseDirty( start: Int ) = start + 1
+
